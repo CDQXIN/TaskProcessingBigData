288882ad618f1b8ec67ae3641a59cd2db25839db
Administrator <donchen1990@163.com>
2019-11-01 17:20:56 +0800

    异步方法

---
+++ TaskProcessingBigData/MethodAsyncSpecial.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskProcessingBigData
+{
+    public class MethodAsyncSpecial
+    {
+        delegate string delegateSayInfo(string info);
+        public string SaySayInfo()
+        {
+            delegateSayInfo delegatesayinfo = new delegateSayInfo(SaySayHello);
+
+            IAsyncResult delegateresult = delegatesayinfo.BeginInvoke("laomaotao", null, null); //会在新线程中执行
+            string resultstr = delegatesayinfo.EndInvoke(delegateresult);
+            return "";
+        }
+
+        public string SaySayHello(string info)
+        {
+            return $"hello,{info}";
+        }
+
+    }
+}
