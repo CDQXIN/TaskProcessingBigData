5583eb0dd5084578a2d076c4fa3603ac75b38455
Administrator <donchen1990@163.com>
2019-11-01 16:38:52 +0800

    提交异步方法

---
+++ TaskProcessingBigData/MethodAsync.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskProcessingBigData
+{
+    public class MethodAsync
+    {
+        /// <summary>
+        /// 异步方法
+        /// </summary>
+        /// <returns></returns>
+        public static async Task<string> GetReturnResult()
+        {
+            return await Task.Run(() =>
+            {
+               
+                return "";
+            });
+        }
+    }
+}
